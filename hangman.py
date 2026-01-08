e9dfe1a310d2ac8e36e0f0724dfcb77b76d53b27
rishi-tech387 <lrishika387@gmail.com>
2026-01-08 13:24:37 +0530

    CODE ADDED

--- hangman.py
+++ hangman.py
@@ -1,1 +1,39 @@
+import random
+
+words = ["apple", "banana", "orange", "grapes", "mango"]
+
+word = random.choice(words)
+guessed_letters = []
+attempts = 6
+
+print("Welcome to Hangman Game")
+
+while attempts > 0:
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter + " "
+        else:
+            display += "_ "
+
+    print("\nWord:", display)
+
+    if "_" not in display:
+        print("You won!")
+        break
+
+    guess = input("Enter a letter: ").lower()
+
+    if guess in guessed_letters:
+        print("Already guessed")
+        continue
+
+    guessed_letters.append(guess)
+
+    if guess not in word:
+        attempts -= 1
+        print("Wrong guess. Attempts left:", attempts)
+
+if attempts == 0:
+    print("You lost. The word was:", word)
 
